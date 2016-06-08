e4ef29c08adfe2198ad143613e71cf1218982fc0
daneryl <daneryl@gmail.com>
2016-06-08 15:10:03 -0500

    database test config

--- app/api/config/database.js
+++ app/api/config/database.js
@@ -1,6 +1,7 @@
+const COUCHDBURL = process.env.COUCHDB_URL;
 export default {
   demo: 'http://127.0.0.1:5984/uwazi_demo',
-  development: process.env.COUCHDB_URL || 'http://127.0.0.1:5984/uwazi_development',
-  testing: 'http://127.0.0.1:5984/uwazi_testing',
+  development: COUCHDBURL ? `${COUCHDBURL}/uwazi_development` : 'http://127.0.0.1:5984/uwazi_development',
+  testing: COUCHDBURL ? `${COUCHDBURL}/uwazi_testing` : 'http://127.0.0.1:5984/uwazi_testing',
   production: 'http://127.0.0.1:5984/uwazi'
 };
