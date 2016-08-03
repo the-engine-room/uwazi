c853e6dd3e1bd460cd1eaeecad83d721fb845ff2
daneryl <daneryl@gmail.com>
2016-08-03 16:20:47 +0200

    changed blank_state script to work with dockerized version

--- couchdb/blank_state.sh
+++ couchdb/blank_state.sh
@@ -8,7 +8,7 @@ sleep 1
 echo -e "\nCreating ${1:-uwazi_development} database"
 curl -X PUT http://127.0.0.1:5984/${1-uwazi_development}/
 echo -e "\ncreating blank state on ${1:-uwazi_development} database"
-../node_modules/couchdb-dump/bin/cdbload -d ${1:-uwazi_development} < uwazi_blank_state.json
+../node_modules/couchdb-dump/bin/cdbload -h couchdb -d ${1:-uwazi_development} < uwazi_blank_state.json
 sleep 1
 echo -e "\nreseting views on ${1:-uwazi_development} database"
 ./restore_views.sh
